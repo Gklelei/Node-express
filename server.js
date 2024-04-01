8f145b3357d2926f2fac283db5015f2b05069c50
gklelei <leleigideon97@gmail.com>
2024-04-01 13:16:00 +0300

    changes to server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const path = require("path");
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3500;
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
