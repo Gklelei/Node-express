a1dd0d6f2b48b14252ecd16ebd6e48a0fc9f90f3
gklelei <leleigideon97@gmail.com>
2024-04-09 12:26:15 +0300

    intro to express

--- server.js
+++ server.js
@@ -3,14 +3,54 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.get("^/$|index.html", (req, res) => {
-  // res.sendFile("./views/index.html", { root: __dirname });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+/* app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+}); */
+app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-app.get("/new-page.html", (req, res) => {
+app.get("/new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
+app.get("old-page(.html)?", (res, req) => {
+  res.redirect(301, "/new-page.html");
+});
 
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Attempt to view hello");
+    next();
+  },
+  (req, res) => {
+    res.send("hello world");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+
+const three = (req, res) => {
+  console.log("three");
+  res.send("finished");
+};
+
+app.get("/chain(.html)?", one, two, three);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+  console.log(`SERVER LISTENING AT PORT ${PORT}`);
 });
--- test.js
+++ test.js
@@ -1,53 +1,47 @@
-const http = require("http");
-const pathModule = require("path"); // Renamed to avoid conflict with path module
-const fs = require("fs");
-const { promisify } = require("util"); // Using promisify to convert fs.readFile to a promise-based function
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
-const logEvents = require("./logEvents");
-const eventsEmitter = require("events");
-class Emitter extends eventsEmitter {}
-const myEmitter = new Emitter();
-
-const PORT = process.env.PORT || 3500;
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-
-  let filePath;
-  if (req.url === "/" || req.url === "/index.html") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    filePath = pathModule.join(__dirname, "Views", "index.html");
-  } else if (req.url === "/style.css") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/css");
-    filePath = pathModule.join(__dirname, "public", "style.css");
-  } else if (req.url === "/script.js") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/javascript");
-    filePath = pathModule.join(__dirname, "public", "script.js");
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Not Found");
-    return;
-  }
-
-  const readFileAsync = promisify(fs.readFile);
-  readFileAsync(filePath, "utf8")
-    .then((data) => {
-      res.end(data);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Internal Server Error");
-    });
-
-  // Log the request
-  myEmitter.emit("log", `Request: ${req.url}, Method: ${req.method}`);
+app.get("^/$|/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+app.get("/old-page", (req, res) => {
+  res.redirect(301, "new-page.html");
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Attemted to view helloow");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello world");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("Two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.send("Hello world!");
+};
+
+app.get("/chain(.html)?", one, two, three);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(PORT, () => {
+  console.log(`SERVER LISTENING AT PORT ${PORT}`);
 });
